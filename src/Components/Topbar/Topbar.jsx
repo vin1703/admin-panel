629ee405e48d4ee9d7ec982da5938895a6e8e558
Vinay <vinaykgarg720@gmail.com>
2024-04-17 14:15:23 +0530

    add Logout

--- src/Components/Topbar/Topbar.jsx
+++ src/Components/Topbar/Topbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './topbar.css'
 import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+import { loginSuccess } from '../../redux/userRedux';
+import { getProductSuccess } from '../../redux/productRedux';
+import {useDispatch} from 'react-redux';
+import {useNavigate} from "react-router-dom"
 
 function Topbar() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogout = () =>{
+    dispatch(loginSuccess(null));
+    dispatch(getProductSuccess([]));
+    navigate('/',{replace:true});
+  }
   return (
 <div className="topbar">
       <div className="topbarWrapper">
@@ -18,9 +29,9 @@ function Topbar() {
             <Language />
             <span className="topIconBadge">2</span>
           </div> */}
-          {/* <div className="topbarIconContainer">
-            <Settings />
-          </div> */}
+          <div onClick={handleLogout} className="topbarIconContainer">
+            <h4>Logout</h4>
+          </div>
           <img src="https://media.licdn.com/dms/image/D5603AQEW-g3BNmsRLw/profile-displayphoto-shrink_800_800/0/1700204649863?e=2147483647&v=beta&t=v_zykYE9LuLcSzzGdILknuC83_4u6hQr738YmNuRSp8" alt="" className="topAvatar" />
         </div>
       </div>
